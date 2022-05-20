f3939442e29c98d22f881bd50ae0ffaaebc32214
christinefrancisco <christine.a.francisco@gmail.com>
2022-05-20 00:57:49 -0400

    fetch api works

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-document.getElementById("submit").addEventListener('click', getFetch)
+document.querySelector("#button").addEventListener('click', getFetch)
 
-// const choice = document.getElementById("prompt").value
 
 const data = {
-    prompt: "Write a poem about a dog wearing skis",
+    prompt: document.getElementById("prompt").value,
     temperature: 0.5,
     max_tokens: 64,
     top_p: 1.0,
@@ -11,20 +10,18 @@ const data = {
     presence_penalty: 0.0,
    };
     
-
 function getFetch(){
-  console.log("oh hi there")
-//   fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${"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"}`,
-//     },
-//     body: JSON.stringify(data),
-//    })
-//         .then(response => response.json())
-//         .then(newData => {
-//             console.log(newData.choices[0].text)
-//         })
+    console.log("oh hi there")
+    fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${"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"}`,
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(newData => {
+            console.log(newData.choices[0].text)
+        })
 }
-console.log("hello")
